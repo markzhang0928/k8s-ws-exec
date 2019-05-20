79cc7965dc76b6900714e2b2825c2d55bb312898
zhangyi7 <zhangyi7@huya.com>
2019-05-20 16:01:56 +0800

    first commit

---
+++ exec.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/gorilla/websocket"
+	"k8s.io/client-go/rest"
+)
+
+// ExecOptions describe a execute request args.
+type ExecOptions struct {
+	Namespace string
+	Pod       string
+	Container string
+	Command   []string
+	TTY       bool
+	Stdin     bool
+}
+
+type RoundTripCallback func(c *websocket.Conn) error
+
+type WebsocketRoundTripper struct {
+	TLSConfig *tls.Config
+	Callback  RoundTripCallback
+}
+
+var cacheBuff bytes.Buffer
+
+var protocols = []string{
+	"v4.channel.k8s.io",
+	"v3.channel.k8s.io",
+	"v2.channel.k8s.io",
+	"channel.k8s.io",
+}
+
+const (
+	stdin = iota
+	stdout
+	stderr
+)
+
+func WebsocketCallback(c *websocket.Conn) error {
+	errChan := make(chan error, 3)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		buf := make([]byte, 1025)
+		for {
+			n, err := os.Stdin.Read(buf[1:])
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			cacheBuff.Write(buf[1:n])
+			cacheBuff.Write([]byte{13, 10})
+			if err := c.WriteMessage(websocket.BinaryMessage, buf[:n+1]); err != nil {
+				errChan <- err
+				return
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for {
+			_, buf, err := c.ReadMessage()
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			if len(buf) > 1 {
+				var w io.Writer
+				switch buf[0] {
+				case stdout:
+					w = os.Stdout
+				case stderr:
+					w = os.Stderr
+				}
+
+				if w == nil {
+					continue
+				}
+				s := strings.Replace(string(buf[1:]), cacheBuff.String(), "", -1)
+				_, err = w.Write([]byte(s))
+				if err != nil {
+					errChan <- err
+					return
+				}
+			}
+			cacheBuff.Reset()
+		}
+	}()
+
+	wg.Wait()
+	close(errChan)
+	err := <-errChan
+	return err
+}
+
+func (wrt *WebsocketRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	dialer := &websocket.Dialer{
+		Proxy:           http.ProxyFromEnvironment,
+		TLSClientConfig: wrt.TLSConfig,
+		Subprotocols:    protocols,
+	}
+
+	conn, resp, err := dialer.Dial(r.URL.String(), r.Header)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	return resp, wrt.Callback(conn)
+}
+
+func ExecRoundTripper(config *rest.Config, f RoundTripCallback) (http.RoundTripper, error) {
+	tlsConfig, err := rest.TLSConfigFor(config)
+	if err != nil {
+		return nil, err
+	}
+
+	rt := &WebsocketRoundTripper{
+		Callback:  f,
+		TLSConfig: tlsConfig,
+	}
+	return rest.HTTPWrappersForConfig(config, rt)
+}
+
+func ExecRequest(config *rest.Config, opts *ExecOptions) (*http.Request, error) {
+	u, err := url.Parse(config.Host)
+	if err != nil {
+		return nil, err
+	}
+
+	switch u.Scheme {
+	case "https":
+		u.Scheme = "wss"
+	case "http":
+		u.Scheme = "ws"
+	default:
+		return nil, fmt.Errorf("Unrecognised URL scheme in %v", u)
+	}
+
+	u.Path = fmt.Sprintf("/api/v1/namespaces/%s/pods/%s/exec", opts.Namespace, opts.Pod)
+
+	rawQuery := "stdout=true&tty=true"
+	for _, c := range opts.Command {
+		rawQuery += "&command=" + c
+	}
+
+	if opts.Container != "" {
+		rawQuery += "&container=" + opts.Container
+	}
+
+	if opts.TTY {
+		rawQuery += "&tty=true"
+	}
+
+	if opts.Stdin {
+		rawQuery += "&stdin=true"
+	}
+	u.RawQuery = rawQuery
+
+	return &http.Request{
+		Method: http.MethodGet,
+		URL:    u,
+	}, nil
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/kubedev/k8s-ws-exec
+
+require (
+	github.com/gorilla/websocket v1.4.0
+	github.com/imdario/mergo v0.3.7 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/pflag v1.0.3
+	golang.org/x/crypto v0.0.0-20190513172903-22d7a77e9e5f // indirect
+	golang.org/x/net v0.0.0-20190514140710-3ec191127204 // indirect
+	golang.org/x/oauth2 v0.0.0-20190517181255-950ef44c6e07 // indirect
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+	k8s.io/apimachinery v0.0.0-20190515023456-b74e4c97951f // indirect
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/utils v0.0.0-20190506122338-8fab8cb257d5 // indirect
+)
